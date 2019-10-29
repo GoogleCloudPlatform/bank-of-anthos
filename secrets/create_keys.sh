11c727312782e915c951c4ac1b4f55df1c86ccda
Daniel Sanche <d.sanche14@gmail.com>
2019-10-29 15:40:04 -0700

    keys are added as k8s secrets

--- secrets/create_keys.sh
+++ secrets/create_keys.sh
@@ -1,9 +1,9 @@
 KEYRING_NAME=financial-app
-PROJECT=<your-project-here>
+PROJECT=sanche-testing-project
 
 gcloud kms keyrings create $KEYRING_NAME  --location global --project $PROJECT
 
-for BANK_NAME in "bank-0" "bank-1"; do
+for BANK_NAME in "bank-0"; do
     gcloud kms keys create $BANK_NAME \
       --location global \
       --keyring $KEYRING_NAME \
@@ -27,6 +27,10 @@ for BANK_NAME in "bank-0" "bank-1"; do
     gcloud iam service-accounts keys create ./$BANK_NAME.json \
       --iam-account $BANK_NAME@$PROJECT.iam.gserviceaccount.com \
       --project $PROJECT
+
+    kubectl create secret generic $BANK_NAME-service-account --from-file=./$BANK_NAME.json
+    echo -n "projects/$PROJECT/locations/global/keyRings/$KEYRING_NAME/cryptoKeys/$BANK_NAME" > $BANK_NAME-key-path.txt
+    kubectl create secret generic $BANK_NAME-key-path --from-file=./$BANK_NAME-key-path.txt
 done
 
 
