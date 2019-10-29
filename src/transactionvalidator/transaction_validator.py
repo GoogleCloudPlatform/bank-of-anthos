efd8ed79c8c8b05712c287db6d9c7012fbcb1c6d
Daniel Sanche <d.sanche14@gmail.com>
2019-10-29 16:49:46 -0700

    sign transactions as they come in

--- src/transactionvalidator/transaction_validator.py
+++ src/transactionvalidator/transaction_validator.py
@@ -4,11 +4,25 @@ import os
 import uuid
 import json
 
+import hashlib
+
+from google.cloud import kms_v1
+
 unconf_host = os.getenv('UNCONF_ADDR')
 unconf_port = os.getenv('UNCONF_PORT')
 stream_name = os.getenv("UNCONF_STREAM")
 
-key_path = os.getenv("KEY_PATH")
+key_path = os.getenv("KEY_PATH").strip()
+
+def sign_transaction(transaction):
+    t = transaction.copy()
+    del t['send_branch_sig']
+    del t['recv_branch_sig']
+    digest_bytes = hashlib.sha256(str(t)).digest()
+    digest_json = {'sha256': digest_bytes}
+    response = client.asymmetric_sign(key_path, digest_json)
+    return response.signature
+
 def add_transaction(from_id, to_id, amount):
     transaction_id = str(uuid.uuid4())
     timestamp = time()
@@ -22,6 +36,9 @@ def add_transaction(from_id, to_id, amount):
                  'time':timestamp,
                  'transaction_id':transaction_id
                 }
+    sig = sign_transaction(trans_obj)
+    trans_obj['send_branch_sig'] = sig
+    trans_obj['recv_branch_sig'] = sig
     r.xadd(stream_name, trans_obj)
     print('added transaction: {}'.format(trans_obj))
 
@@ -29,6 +46,7 @@ if __name__ == '__main__':
     print('host: {} port: {}'.format(unconf_host, unconf_port))
 
     r = redis.Redis(host=unconf_host, port=unconf_port, db=0)
+    client = kms_v1.KeyManagementServiceClient()
 
     while True:
         add_transaction('dan', 'sanche', 10)
