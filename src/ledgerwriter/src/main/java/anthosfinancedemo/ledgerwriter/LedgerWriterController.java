9b9199acd0095e900297b8ce410d4ed65369999c
Daniel Sanche <d.sanche14@gmail.com>
2020-01-22 16:59:52 -0800

    implemented readiness probe in ledgerwriter java

--- src/ledgerwriter/src/main/java/anthosfinancedemo/ledgerwriter/LedgerWriterController.java
+++ src/ledgerwriter/src/main/java/anthosfinancedemo/ledgerwriter/LedgerWriterController.java
@@ -1,11 +1,20 @@
 package anthosfinancedemo.ledgerwriter;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 public class LedgerWriterController {
+
+  @GetMapping("/ready")
+  @ResponseStatus(HttpStatus.OK)
+  public final String readiness() {
+      return "ok";
+  }
 
   @PostMapping("/new_transaction")
   public final void addTransaction(@RequestBody Transaction transaction) {
