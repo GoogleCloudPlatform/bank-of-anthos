f2566050131f25512cbca5ed8cc2d43753a07e5d
Daniel Sanche <d.sanche14@gmail.com>
2020-01-28 16:23:15 -0800

    verify token

--- src/ledgerwriter/src/main/java/anthosfinancedemo/ledgerwriter/LedgerWriterController.java
+++ src/ledgerwriter/src/main/java/anthosfinancedemo/ledgerwriter/LedgerWriterController.java
@@ -5,7 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+
 
 @RestController
 public class LedgerWriterController {
@@ -21,7 +42,41 @@ public class LedgerWriterController {
   }
 
   @PostMapping("/new_transaction")
-  public final void addTransaction(@RequestBody Transaction transaction) {
+  public final void addTransaction(@RequestHeader("Authorization") String bearerToken,
+                                   @RequestBody Transaction transaction) throws IOException,
+                                   NoSuchAlgorithmException, InvalidKeySpecException {
+    if (bearerToken != null && bearerToken.startsWith("Bearer ")){
+      bearerToken = bearerToken.split("Bearer ")[1];
+    }
+    System.out.println(bearerToken);
+
+    String pubKeyPath = System.getenv("PUB_KEY_PATH");
+    System.out.println(pubKeyPath);
+    byte[] pubKeyBytes  = Files.readAllBytes(Paths.get(pubKeyPath));
+    String pubKeyStr = new String (pubKeyBytes);
+    //pubKeyStr = pubKeyStr.split(" ")[1];
+    System.out.println(pubKeyStr);
+
+    pubKeyStr = pubKeyStr.replaceFirst("-----BEGIN PUBLIC KEY-----", "");
+    pubKeyStr = pubKeyStr.replaceFirst("-----END PUBLIC KEY-----", "");
+    pubKeyStr = pubKeyStr.replaceAll("\\s", "");
+    System.out.println(pubKeyStr);
+    pubKeyBytes = Base64.getDecoder().decode(pubKeyStr);
+
+    KeyFactory kf = KeyFactory.getInstance("RSA");
+    X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(pubKeyBytes);
+    RSAPublicKey pubKey = (RSAPublicKey) kf.generatePublic(keySpecX509);
+
+    try {
+        Algorithm algorithm = Algorithm.RSA256(pubKey, null);
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        DecodedJWT jwt = verifier.verify(bearerToken);
+        System.out.println("success");
+    } catch (JWTVerificationException exception){
+        //Invalid signature/claims
+        System.out.println("error");
+        System.out.println(exception);
+    }
     // TODO: Do stuff
     //
     // Get the auth header.
