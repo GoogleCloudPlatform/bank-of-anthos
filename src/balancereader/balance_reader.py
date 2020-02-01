a1d76d555c5377ee11170c258ad8247e4740dad5
Daniel Sanche <d.sanche14@gmail.com>
2020-01-31 16:08:09 -0800

    fixed Python style issues

--- src/balancereader/balance_reader.py
+++ src/balancereader/balance_reader.py
@@ -11,23 +11,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+Balancereader keeps a read-only cache of the balances of each address
+in the ledger
+"""
 
 import logging
 import os
+import sys
 import threading
 from collections import defaultdict
 
 from flask import Flask, jsonify, request
-
 import jwt
-
 import redis
 
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
 
-@app.route('/ready', methods=['GET'])
+@APP.route('/ready', methods=['GET'])
 def readiness():
     """
     Readiness probe
@@ -35,18 +38,17 @@ def readiness():
     return 'ok', 200
 
 
-@app.route('/healthy', methods=['GET'])
+@APP.route('/healthy', methods=['GET'])
 def liveness():
     """
     Liveness probe. Fail if background thread dies
     """
-    if _bg_thread is not None and _bg_thread.is_alive():
+    if BACKGROUND_THREAD is not None and BACKGROUND_THREAD.is_alive():
         return 'ok', 200
-    else:
-        return 'error', 500
+    return 'error', 500
 
 
-@app.route('/get_balance', methods=['GET'])
+@APP.route('/get_balance', methods=['GET'])
 def get_balance():
     """
     Returns snapshot of user's balance
@@ -59,13 +61,13 @@ def get_balance():
     else:
         token = ''
     try:
-        payload = jwt.decode(token, key=_public_key, algorithms='RS256')
+        payload = jwt.decode(token, key=PUBLIC_KEY, algorithms='RS256')
         account_id = payload['acct']
-        balance = _balance_dict[account_id]
+        balance = BALANCE_DICT[account_id]
         return jsonify({'balance': balance}), 200
-    except jwt.exceptions.InvalidTokenError as e:
-        logging.error(e)
-        return jsonify({'error': str(e)}), 401
+    except jwt.exceptions.InvalidTokenError as err:
+        logging.error(err)
+        return jsonify({'error': str(err)}), 401
 
 
 def _process_transaction(transaction):
@@ -77,10 +79,10 @@ def _process_transaction(transaction):
     receiver_acct = transaction['toAccountNum']
     receiver_route = transaction['toRoutingNum']
     amount = int(transaction['amount'])
-    if sender_route == _local_routing_num:
-        _balance_dict[sender_acct] -= amount
-    if receiver_route == _local_routing_num:
-        _balance_dict[receiver_acct] += amount
+    if sender_route == LOCAL_ROUTING:
+        BALANCE_DICT[sender_acct] -= amount
+    if receiver_route == LOCAL_ROUTING:
+        BALANCE_DICT[receiver_acct] += amount
 
 
 def _query_transactions(last_transaction_id=b'0-0', block=True):
@@ -94,13 +96,13 @@ def _query_transactions(last_transaction_id=b'0-0', block=True):
         # don't block
         block_time = None
 
-    new_set = _ledger.xread({_ledger_stream: last_transaction_id},
-                            block=block_time)
+    new_set = LEDGER.xread({LEDGER_STREAM: last_transaction_id},
+                           block=block_time)
     if len(new_set) > 0:
         for entry in new_set[0][1]:
             last_transaction_id = entry[0]
             transaction = entry[1]
-            logging.info('processing transaction: {}'.format(transaction))
+            logging.info('processing transaction: %s', transaction)
             _process_transaction(transaction)
     return last_transaction_id
 
@@ -121,27 +123,27 @@ if __name__ == '__main__':
               'LOCAL_ROUTING_NUM', 'PUB_KEY_PATH']:
         if os.environ.get(v) is None:
             print("error: {} environment variable not set".format(v))
-            exit(1)
+            sys.exit(1)
 
     # setup global variables
-    _balance_dict = defaultdict(int)
-    _public_key = open(os.environ.get('PUB_KEY_PATH'), 'r').read()
-    _ledger_stream = os.getenv('LEDGER_STREAM')
-    _ledger = redis.Redis(host=os.getenv("LEDGER_ADDR"),
-                          port=os.getenv("LEDGER_PORT"), db=0)
-    _local_routing_num = os.getenv('LOCAL_ROUTING_NUM')
+    BALANCE_DICT = defaultdict(int)
+    PUBLIC_KEY = open(os.environ.get('PUB_KEY_PATH'), 'r').read()
+    LEDGER_STREAM = os.getenv('LEDGER_STREAM')
+    LEDGER = redis.Redis(host=os.getenv("LEDGER_ADDR"),
+                         port=os.getenv("LEDGER_PORT"), db=0)
+    LOCAL_ROUTING = os.getenv('LOCAL_ROUTING_NUM')
 
     # build balance dictionary
     logging.info('restoring balances...')
-    last_transaction_id = _query_transactions(block=False)
+    CACHE_RESTORE_POINT = _query_transactions(block=False)
 
     # start background transaction listener thread
     logging.info('starting transaction listener thread...')
-    _bg_thread = threading.Thread(target=transaction_listener,
-                                  args=[last_transaction_id])
-    _bg_thread.daemon = True
-    _bg_thread.start()
+    BACKGROUND_THREAD = threading.Thread(target=transaction_listener,
+                                         args=[CACHE_RESTORE_POINT])
+    BACKGROUND_THREAD.daemon = True
+    BACKGROUND_THREAD.start()
 
     # start serving requests
     logging.info("starting flask...")
-    app.run(debug=False, port=os.environ.get('PORT'), host='0.0.0.0')
+    APP.run(debug=False, port=os.environ.get('PORT'), host='0.0.0.0')
