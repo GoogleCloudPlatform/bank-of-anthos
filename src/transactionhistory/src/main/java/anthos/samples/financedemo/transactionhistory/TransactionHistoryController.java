bd5bfbc3896e80b1eb3af1c53de41e9d3946e4d6
Daniel Sanche <d.sanche14@gmail.com>
2020-02-03 16:53:02 -0800

    added skeleton code for transaction history

---
+++ src/transactionhistory/src/main/java/anthos/samples/financedemo/transactionhistory/TransactionHistoryController.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2020, Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package anthos.samples.financedemo.transactionhistory;
+
+import io.lettuce.core.api.StatefulRedisConnection;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+
+@RestController
+public final class TransactionHistoryController {
+
+    ApplicationContext ctx =
+            new AnnotationConfigApplicationContext(TransactionHistoryConfig.class);
+
+    private final String ledgerStreamKey = System.getenv("LEDGER_STREAM");
+    private final String routingNum =  System.getenv("LOCAL_ROUTING_NUM");
+    private final JWTVerifier verifier;
+
+    public TransactionHistoryController() throws IOException,
+                                           NoSuchAlgorithmException,
+                                           InvalidKeySpecException {
+        // load public key from file
+        String fPath = System.getenv("PUB_KEY_PATH");
+        String pubKeyStr  = new String(Files.readAllBytes(Paths.get(fPath)));
+        pubKeyStr = pubKeyStr.replaceFirst("-----BEGIN PUBLIC KEY-----", "");
+        pubKeyStr = pubKeyStr.replaceFirst("-----END PUBLIC KEY-----", "");
+        pubKeyStr = pubKeyStr.replaceAll("\\s", "");
+        byte[] pubKeyBytes = Base64.getDecoder().decode(pubKeyStr);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(pubKeyBytes);
+        RSAPublicKey publicKey = (RSAPublicKey) kf.generatePublic(keySpecX509);
+        // set up verifier
+        Algorithm algorithm = Algorithm.RSA256(publicKey, null);
+        this.verifier = JWT.require(algorithm).build();
+    }
+
+    /**
+     * Readiness probe endpoint.
+     *
+     * @return HTTP Status 200 if server is serving requests.
+     */
+    @GetMapping("/ready")
+    @ResponseStatus(HttpStatus.OK)
+    public String readiness() {
+        return "ok";
+    }
+
+    /**
+     * Liveness probe endpoint.
+     *
+     * @return HTTP Status 200 if server is healthy.
+     */
+    @GetMapping("/healthy")
+    @ResponseStatus(HttpStatus.OK)
+    public String liveness() {
+        return "ok";
+    }
+
+}
