8fb416e8e6d6b09d831da64c8114411e6d7b9136
Daniel Sanche <d.sanche14@gmail.com>
2020-03-04 18:18:42 -0800

    reorganized release script

--- release/make-release.sh
+++ release/make-release.sh
@@ -9,28 +9,30 @@ CURRENT_VERSION=$(grep -A 1 VERSION $REPO_ROOT/kubernetes-manifests/*.yaml | gre
 NEW_VERSION=v1.0.0
 
 if [[ $(git rev-parse origin/master) != $(git rev-parse @) ]]; then
-    echo "error: must be up to date with origin/master"
+    echo "error: must be on same commit as origin/master"
     exit 1
 fi
 if [[ $(git status -s | wc -l) -gt 0 ]]; then
     echo "error: can't have unstaged changes"
     exit 1
 fi
-echo $CURRENT_BRANCH
 
+exit 1
 
 # update version in manifests
 find $REPO_ROOT/kubernetes-manifests -name '*.yaml' -exec sed -i -e "s/$CURRENT_VERSION/\"$NEW_VERSION\"/g" {} \;
 
-# build and push containers
-#skaffold build --default-repo=gcr.io/bank-of-anthos --tag=$NEW_VERSION
-
 # push release PR
-#git checkout -b release/$NEW_VERSION
-#git add $REPO_ROOT/kubernetes-manifests/*.yaml
-#git commit -m "release/$NEW_VERSION"
-#git push --set-upstream origin release/$NEW_VERSION
+git checkout -b release/$NEW_VERSION
+git add $REPO_ROOT/kubernetes-manifests/*.yaml
+git commit -m "release/$NEW_VERSION"
 
-# push tag
-#git tag $NEW_VERSION
-#git push --tags
+# add tag
+git tag $NEW_VERSION
+
+# build and push containers
+skaffold build --default-repo=gcr.io/bank-of-anthos --tag=$NEW_VERSION
+
+# push to repo
+git push --set-upstream origin release/$NEW_VERSION
+git push --tags
