05882257680f11a55c20d3230ed1c0b85a0e60a6
David Mailhot <djmailhot@gmail.com>
2020-04-30 13:53:47 -0700

    Update accounts-db default data init script to hardcode data

--- src/accounts-db/initdb/1-load-testdata.sh
+++ src/accounts-db/initdb/1-load-testdata.sh
@@ -28,49 +28,52 @@ readonly ENV_VARS=(
   "POSTGRES_DB"
   "POSTGRES_USER"
   "LOCAL_ROUTING_NUM"
-  "DEFAULT_USER_ACCOUNT"
-  "DEFAULT_USER_USERNAME"
 )
 
 
 add_user() {
-	psql -X -v ON_ERROR_STOP=1 -v account="$1" -v username="$2" -v firstname="$3" --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-		INSERT INTO users VALUES (:'account', :'username', '\x243262243132245273764737474f39777562452f4a786a79444a7263756f386568466b762e634e5262356e6867746b474752584c6634437969346643', :'firstname', 'Testuser', '2000-01-01', '-5', 'Bowling Green, New York City', 'NY', '10004', '111-22-3333') ON CONFLICT DO NOTHING;
+  # Usage:  add_user "ACCOUNTID" "USERNAME" "FIRST_NAME"
+  psql -X -v ON_ERROR_STOP=1 -v account="$1" -v username="$2" -v firstname="$3" -v passhash="$DEFAULT_PASSHASH" --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    INSERT INTO users VALUES (:'account', :'username', :'passhash', :'firstname', 'Testuser', '2000-01-01', '-5', 'Bowling Green, New York City', 'NY', '10004', '111-22-3333') ON CONFLICT DO NOTHING;
 EOSQL
 }
 
 
 add_external_account() {
-	psql -X -v ON_ERROR_STOP=1 -v username="$1" -v label="$2" -v account="$3" -v routing="$4" --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-		INSERT INTO contacts VALUES (:'username', :'label', :'account', :'routing', 'true') ON CONFLICT DO NOTHING;
+  # Usage:  add_external_account "OWNER_USERNAME" "LABEL" "ACCOUNT" "ROUTING"
+  psql -X -v ON_ERROR_STOP=1 -v username="$1" -v label="$2" -v account="$3" -v routing="$4" --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    INSERT INTO contacts VALUES (:'username', :'label', :'account', :'routing', 'true') ON CONFLICT DO NOTHING;
 EOSQL
 }
 
 
 add_contact() {
-	psql -X -v ON_ERROR_STOP=1 -v username="$1" -v label="$2" -v account="$3" -v routing="$LOCAL_ROUTING_NUM" --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-		INSERT INTO contacts VALUES (:'username', :'label', :'account', :'routing', 'false') ON CONFLICT DO NOTHING;
+  # Usage:  add_contact "OWNER_USERNAME" "CONTACT_LABEL" "CONTACT_ACCOUNT"
+  psql -X -v ON_ERROR_STOP=1 -v username="$1" -v label="$2" -v account="$3" -v routing="$LOCAL_ROUTING_NUM" --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    INSERT INTO contacts VALUES (:'username', :'label', :'account', :'routing', 'false') ON CONFLICT DO NOTHING;
 EOSQL
 }
 
 
 # Load test data into the database
-load_testdata() {
-  # Add the default user with an external deposit account and contacts A & B
-  add_user "$DEFAULT_USER_ACCOUNT" "$DEFAULT_USER_USERNAME" "$DEFAULT_USER_NAME"
-  add_external_account "$DEFAULT_USER_USERNAME" "$DEFAULT_DEPOSIT_LABEL" "$DEFAULT_DEPOSIT_ACCOUNT" "$DEFAULT_DEPOSIT_ROUTING"
-  add_contact "$DEFAULT_USER_USERNAME" "$DEFAULT_CONTACT_NAME_A" "$DEFAULT_CONTACT_ACCOUNT_A"
-  add_contact "$DEFAULT_USER_USERNAME" "$DEFAULT_CONTACT_NAME_B" "$DEFAULT_CONTACT_ACCOUNT_B"
+create_accounts() {
+  # Add default users.
+  add_user "1011226111" "alice" "Alice"
+  add_user "1033623433" "bob" "Bob"
+  add_user "1055757655" "eve" "Eve"
 
-  # Add contact A as a user with the default user as a contact
-  add_user "$DEFAULT_CONTACT_ACCOUNT_A" "$DEFAULT_CONTACT_NAME_A" "$DEFAULT_CONTACT_NAME_A"
-  add_external_account "$DEFAULT_CONTACT_NAME_A" "$DEFAULT_DEPOSIT_LABEL" "$DEFAULT_DEPOSIT_ACCOUNT" "$DEFAULT_DEPOSIT_ROUTING"
-  add_contact "$DEFAULT_CONTACT_NAME_A" "$DEFAULT_USER_NAME" "$DEFAULT_USER_ACCOUNT"
+  # Make everyone contacts of each other
+  add_contact "alice" "Bob" "1033623433"
+  add_contact "alice" "Eve" "1055757655"
+  add_contact "bob" "Alice" "1011226111",
+  add_contact "bob" "Eve" "1055757655"
+  add_contact "eve" "Bob" "1033623433"
+  add_contact "eve" "Alice" "1011226111",
 
-  # Add contact B as a user with the default user as a contact
-  add_user "$DEFAULT_CONTACT_ACCOUNT_B" "$DEFAULT_CONTACT_NAME_B" "$DEFAULT_CONTACT_NAME_B"
-  add_external_account "$DEFAULT_CONTACT_NAME_B" "$DEFAULT_DEPOSIT_LABEL" "$DEFAULT_DEPOSIT_ACCOUNT" "$DEFAULT_DEPOSIT_ROUTING"
-  add_contact "$DEFAULT_CONTACT_NAME_B" "$DEFAULT_USER_NAME" "$DEFAULT_USER_ACCOUNT"
+  # Add external accounts
+  add_external_account "alice" "External Bank" "8088895188" "987654321"
+  add_external_account "bob" "External Bank" "8088895188" "987654321"
+  add_external_account "eve" "External Bank" "8088895188" "987654321"
 }
 
 
@@ -83,7 +86,11 @@ main() {
     fi
   done
 
-  load_testdata
+  # A password hash + salt for the default password 'password'
+  # Via Python3:  bycrypt.hashpw('password'.encode('utf-8'), bcrypt.gensalt())
+  DEFAULT_PASSHASH='\x243262243132245273764737474f39777562452f4a786a79444a7263756f386568466b762e634e5262356e6867746b474752584c6634437969346643'
+
+  create_accounts
 }
 
 
