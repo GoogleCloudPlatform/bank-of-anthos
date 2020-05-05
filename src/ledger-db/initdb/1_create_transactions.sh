3c1ac640c3208deb1988e3d0d206669fe041d560
David Mailhot <djmailhot@gmail.com>
2020-05-05 15:48:59 -0700

    Update ledger-db default data init script to hardcode data

--- src/ledger-db/initdb/1_create_transactions.sh
+++ src/ledger-db/initdb/1_create_transactions.sh
@@ -28,6 +28,14 @@ if [ "$USE_DEFAULT_DATA" != "True"  ]; then
 fi
 
 
+# Expected environment variables
+readonly ENV_VARS=(
+  "POSTGRES_DB"
+  "POSTGRES_USER"
+  "LOCAL_ROUTING_NUM"
+)
+
+
 add_transaction() {
     DATE=$(date -u +"%Y-%m-%d %H:%M:%S.%3N%z" --date="@$(($6))")
     echo "adding default transaction: $1 -> $2"
@@ -38,29 +46,67 @@ EOSQL
 }
 
 
-PAY_PERIODS=3
-DAYS_BETWEEN_PAY=14
-SECONDS_IN_PAY_PERIOD=$(( 86400 * $DAYS_BETWEEN_PAY  ))
-echo $SECONDS_IN_PAY_PERIOD
-DEPOSIT_AMOUNT=250000
-PAYMENT_ACCOUNTS=("$DEFAULT_CONTACT_ACCOUNT_A" "$DEFAULT_CONTACT_ACCOUNT_B")
+create_transactions() {
+    PAY_PERIODS=3
+    DAYS_BETWEEN_PAY=14
+    SECONDS_IN_PAY_PERIOD=$(( 86400 * $DAYS_BETWEEN_PAY  ))
+    DEPOSIT_AMOUNT=250000
+    PAYMENT_ACCOUNTS=("$DEFAULT_CONTACT_ACCOUNT_A" "$DEFAULT_CONTACT_ACCOUNT_B")
 
-# create a UNIX timestamp in seconds since the Epoch
-START_TIMESTAMP=$(( $(date +%s) - $(( $(($PAY_PERIODS+1)) * $SECONDS_IN_PAY_PERIOD  ))  ))
+    # create a UNIX timestamp in seconds since the Epoch
+    START_TIMESTAMP=$(( $(date +%s) - $(( $(($PAY_PERIODS+1)) * $SECONDS_IN_PAY_PERIOD  ))  ))
 
-for i in $(seq 1 $PAY_PERIODS); do
-    # create deposit transaction
-    add_transaction "$DEFAULT_DEPOSIT_ACCOUNT" "$DEFAULT_USER_ACCOUNT" "$DEFAULT_DEPOSIT_ROUTING" "$LOCAL_ROUTING_NUM" $DEPOSIT_AMOUNT $START_TIMESTAMP
+    for i in $(seq 1 $PAY_PERIODS); do
+        # create deposit transaction for each user
+        for account in ${USER_ACCOUNTS[@]}; do
+            add_transaction "$EXTERNAL_ACCOUNT" "$account" "$EXTERNAL_ROUTING" "$LOCAL_ROUTING_NUM" $DEPOSIT_AMOUNT $START_TIMESTAMP
+        done
 
-    # create payments
-    TRANSACTIONS_PER_PERIOD=$(shuf -i 3-11 -n1)
-    for p in $(seq 1 $TRANSACTIONS_PER_PERIOD); do
-        AMOUNT=$(shuf -i 100-25000 -n1)
-        RECIPIENT_ACCOUNT=${PAYMENT_ACCOUNTS[$RANDOM % ${#PAYMENT_ACCOUNTS[@]}]}
-        TIMESTAMP=$(( $START_TIMESTAMP + $(( $SECONDS_IN_PAY_PERIOD * $p / $(($TRANSACTIONS_PER_PERIOD + 1 )) )) ))
+        # create payments between users
+        TRANSACTIONS_PER_PERIOD=$(shuf -i 3-11 -n1)
+        for p in $(seq 1 $TRANSACTIONS_PER_PERIOD); do
+            AMOUNT=$(shuf -i 100-25000 -n1)
 
-        add_transaction "$DEFAULT_USER_ACCOUNT" "$RECIPIENT_ACCOUNT" "$LOCAL_ROUTING_NUM" "$LOCAL_ROUTING_NUM" $AMOUNT $TIMESTAMP
+            # randomly select a sender and receiver
+            SENDER_ACCOUNT=${USER_ACCOUNTS[$RANDOM % ${#USER_ACCOUNTS[@]}]}
+            RECIPIENT_ACCOUNT=${USER_ACCOUNTS[$RANDOM % ${#USER_ACCOUNTS[@]}]}
+            # if sender equals receiver, send to a random anonymous account
+            if [[ "$SENDER_ACCOUNT" == "$RECIPIENT_ACCOUNT" ]]; then
+                RECIPIENT_ACCOUNT=$(shuf -i 1000000000-9999999999 -n1)
+            fi
+
+            TIMESTAMP=$(( $START_TIMESTAMP + $(( $SECONDS_IN_PAY_PERIOD * $p / $(($TRANSACTIONS_PER_PERIOD + 1 )) )) ))
+
+            add_transaction "$SENDER_ACCOUNT" "$RECIPIENT_ACCOUNT" "$LOCAL_ROUTING_NUM" "$LOCAL_ROUTING_NUM" $AMOUNT $TIMESTAMP
+        done
+
+        START_TIMESTAMP=$(( $START_TIMESTAMP + $(( $i * $SECONDS_IN_PAY_PERIOD  )) ))
     done
+}
 
-    START_TIMESTAMP=$(( $START_TIMESTAMP + $(( $i * $SECONDS_IN_PAY_PERIOD  )) ))
-done
+
+create_ledger() {
+  # Account numbers for users 'alice', 'bob', and 'eve'.
+  USER_ACCOUNTS=("1011226111", "1033623433", "1055757655")
+  # Numbers for external account 'External Bank'
+  EXTERNAL_ACCOUNT="8088895188"
+  EXTERNAL_ROUTING="987654321"
+
+  create_transactions
+}
+
+
+main() {
+  # Check environment variables are set
+	for env_var in ${ENV_VARS[@]}; do
+    if [[ -z "${!env_var}" ]]; then
+      echo "Error: environment variable '$env_var' not set. Aborting."
+      exit 1
+    fi
+  done
+
+  create_ledger
+}
+
+
+main
