d8d1b5811b9079eba1faed2084d32f493042010c
Guillermina Muro <guillermina.muro@gmail.com>
2020-05-15 21:42:07 -0700

    initial deposit tests

---
+++ .github/workflows/ui-tests/cypress/integration/deposit_spec.js
@@ -0,0 +1,77 @@
+const defaultUser = Cypress.env('defaultUser')
+const username = defaultUser.username
+const password = defaultUser.password
+const name = defaultUser.name
+
+describe('Default user can deposit funds', function() {
+  beforeEach(function() {
+    cy.login(username, password)
+  })
+  
+  it('sees deposit button', function() {
+    cy.get('.h5.mb-0').first().contains('Deposit Funds')
+  })
+
+  it('clicking deposit button makes modal visible', function() {
+    cy.get('#depositFunds').should('not.be.visible')
+    cy.get('.h5.mb-0').first().click() 
+    cy.get('#depositFunds').should('be.visible')
+  })
+  it('shows expected external account', function() {
+    // TODO: add id
+    cy.get('.h5.mb-0').first().click() 
+    cy.get('#depositFunds').should('be.visible') 
+    // TODO: change to deposit accounts
+    cy.get('#accounts').children().first().contains('External Bank')
+    cy.get('#accounts').children().first().contains('9099791699')
+    cy.get('#accounts').children().first().contains('808889588')
+  })
+  it('can deposit funds', function() {
+    // cy.get('.h5.mb-0').first().click() 
+    // cy.get('#depositFunds').should('be.visible') 
+    // // select first account
+
+    // cy.get('#accounts').select('{"account_num": "9099791699", "routing_num": "808889588" }')
+    // cy.get('#deposit-amount').type('5.00')
+    // cy.get('#deposit-form').submit()
+    const externalAccount = {
+        accountNum: '9099791699',
+        routingNum: '808889588'
+    }
+    const depositAmount = 10
+
+    cy.deposit(externalAccount, depositAmount)
+  })
+
+  it.only('can see balance update', function() {
+    cy.get("#current-balance").then(($span) => {
+        const currentBalanceSpan= $span.text()
+        const externalAccount = {
+            accountNum: '9099791699',
+            routingNum: '808889588'
+        }
+        const depositAmount = 300
+        // regex: removes any characters that are not a digit [0-9] or a period [.]
+        const currentBalance = parseFloat(currentBalanceSpan.replace(/[^\d.]/g, ''))
+        const expectedBalance = currentBalance + depositAmount
+        cy.deposit(externalAccount, depositAmount)
+        cy.get('#current-balance').should('contain', expectedBalance)
+        // cy.wrap(updatedBalance).should('contain', expectedBalance)
+      })
+
+  })
+
+  it('can see transaction in history', function() {
+    cy.get('#transaction-table').children().should('have.length.greaterThan', 1)
+  })
+
+  it('see new contact show up', function() {
+
+  })
+// See balance update
+// See transaction in history
+// See new contact show up
+
+
+
+})
--- .github/workflows/ui-tests/cypress/support/commands.js
+++ .github/workflows/ui-tests/cypress/support/commands.js
@@ -43,3 +43,34 @@ Cypress.Commands.add('createAccount', (user) => {
     cy.get('#signup-birthday').type('1981-01-01')
     cy.get('#signup-form').submit()
 })
+
+Cypress.Commands.add('deposit', (externalAccount, depositAmount) => {
+    Cypress.log({
+        name: 'deposit',
+        message: `${externalAccount}` | `${depositAmount}`
+    })
+
+    const accountNum = externalAccount.accountNum
+    const routingNum = externalAccount.routingNum
+
+    // const template = '{"account_num": "${account_num}", "routing_num": "${routing_num}"}';
+    // const makeVal = new Function("{account_num, routing_num}", "return `" + template + "`;")
+    // const val = makeVal({account_num: accountNum, routing_num: routingNum})
+// var makeUrl = new Function("{name, age}", "return `" + template + "`;");
+// var url = makeUrl({name: "John", age: 30});
+// console.log(url); //http://example.com/?name=John&age=30
+
+    cy.get('.h5.mb-0').first().click() 
+    cy.get('#depositFunds').should('be.visible') 
+    // cy.get('#accounts').select(val)
+    cy.get('#accounts').select('{"account_num": "9099791699", "routing_num": "808889588" }')
+    // cy.get('#accounts').select('{"account_num": "9099791699", "routing_num": "808889588" }')
+    // cy.get('#accounts').select(`{"account_num": "${accountNum}", "routing_num": "${routingNum}"}`)
+
+    // cy.get('#accounts').find('option').as('selectOption').then( () => {
+    //         cy.get('#accounts').select(`${this.selectOption.text()}`)
+    // })
+
+    cy.get('#deposit-amount').type(`${depositAmount}`)
+    cy.get('#deposit-form').submit()
+})
