8a93fe94a4f1b966676d51e69dc65ad64a289212
Mike! <ensor@google.com>
2020-12-08 10:01:49 -0800

    initial commit of files from ACM demo. These files are hand-copied, not using the automation

---
+++ extras/acm-multi-repo-demo/deploy-updated-services.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# The purpose of this script is to copy the generated kubernetes manifest files from the primary build process
+# an place them in the opinionated namespace-per-service format for the ACM Namespace repositories
+
