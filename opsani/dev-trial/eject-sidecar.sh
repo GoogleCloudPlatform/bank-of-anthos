be75348b8aa0700d1bf9c0ebab8400200de73f18
Robert Starmer <rstarmer@gmail.com>
2021-02-05 13:19:07 -0800

    Update opsani dev-trial resources

---
+++ opsani/dev-trial/eject-sidecar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source bank-of-anthos.source
+
+echo "Updating deployment removing the envoy sidecar"
+./update-svc.sh app
+kubectl exec -c servo deploy/servo -- servo --token-file /servo/opsani.token eject-sidecar -n ${NAMESPACE} deployment/${DEPLOYMENT}
