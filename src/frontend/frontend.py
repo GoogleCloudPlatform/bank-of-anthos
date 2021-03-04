18be50ccdb856d2db4c75b3a1c4225a09f622cde
askmeegs <megan037@gmail.com>
2021-03-04 11:27:35 -0500

    Frontend

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -547,10 +547,24 @@ def create_app():
     app.jinja_env.globals.update(format_timestamp_month=format_timestamp_month)
     app.jinja_env.globals.update(format_timestamp_day=format_timestamp_day)
 
-    # Set up logging
-    app.logger.handlers = logging.getLogger('gunicorn.error').handlers
-    app.logger.setLevel(logging.getLogger('gunicorn.error').level)
-    app.logger.info('Starting frontend service.')
+    # set log formatting
+    date_format = "%Y-%m-%d %H:%M:%S"
+    message_format = '%(asctime)s | [%(levelname)s] | %(funcName)s | %(message)s'
+    logging.basicConfig(format= message_format, datefmt= date_format, stream=sys.stdout)
+
+    # set log level
+    log_levels = {"DEBUG": logging.DEBUG, "WARNING": logging.WARNING, "INFO": logging.INFO, "ERROR": logging.ERROR, "CRITICAL": logging.CRITICAL}
+    level = logging.INFO #default
+    user_log_level = os.environ["LOG_LEVEL"]
+    user_log_level = user_log_level.upper()
+    if user_log_level in log_levels:
+        level = log_levels[user_log_level]
+    app.logger.setLevel(level)
+
+    # init logger
+    handler = logging.StreamHandler(sys.stdout)
+    app.logger.addHandler(handler)
+    app.logger.info("Starting frontend.")
 
     # Set up tracing and export spans to Cloud Trace.
     if os.environ['ENABLE_TRACING'] == "true":
