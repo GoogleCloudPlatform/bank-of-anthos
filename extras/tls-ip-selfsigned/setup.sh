c8d8668756868740421d02012ec2b428b245315a
shabirmean <7249208+Shabirmean@users.noreply.github.com>
2021-04-19 12:22:49 -0400

    Split resources into seperate yamls

--- extras/tls-ip-selfsigned/setup.sh
+++ extras/tls-ip-selfsigned/setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 gcloud config set project $PROJECT_ID
 
@@ -25,10 +37,11 @@ kubectl create secret tls frontend-tls --cert=public.crt --key=private.key
 kubectl apply -f ../../extras/jwt/jwt-secret.yaml
 kubectl apply -f ../../kubernetes-manifests
 
-######################################################################
-# This creates a Frontendconfig for HTTP->HTTPS redirection, modifies
-# the default frontend Service to be in Internal Loadbalancer
-# (default is External) and finally creates the Ingress that uses the
-# certificates created above
-######################################################################
-kubectl apply -f selfsigned-cert-ingress.yaml
+# Modify the frontend service to use an Internal LoadBalancer to disallow public traffic
+kubectl apply -f frontend-service.yaml
+
+# Apply frontend config to allow HTTP -> HTTPS redirect
+kubectl apply -f frontend-config.yaml
+
+# Create ingress, referencing the cert secret and static IP
+kubectl apply -f frontend-ingress.yaml
