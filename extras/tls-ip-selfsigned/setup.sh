e37e771e52aa653e2aac1247e6346cca4d111987
shabirmean <7249208+Shabirmean@users.noreply.github.com>
2021-04-19 12:10:46 -0400

    Remove Nodeport and add annotation to Service

--- extras/tls-ip-selfsigned/setup.sh
+++ extras/tls-ip-selfsigned/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-gcloud config set project $PROJECT_ID 
+gcloud config set project $PROJECT_ID
 
 # Create static IP
 gcloud compute addresses create boa-single-cluster-ip --global
@@ -9,7 +9,7 @@ gcloud compute addresses create boa-single-cluster-ip --global
 # Store the value of the static IP as a variable
 IP=`gcloud compute addresses describe boa-single-cluster-ip --global --format="value(address)"`
 
-# Create cert 
+# Create cert
 openssl genrsa -out private.key 2048
 
 openssl req -new -key private.key -out frontend.csr \
@@ -18,23 +18,17 @@ openssl req -new -key private.key -out frontend.csr \
 openssl x509 -req -days 365 -in frontend.csr -signkey private.key \
     -out public.crt
 
-# Create secret from cert 
+# Create secret from cert
 kubectl create secret tls frontend-tls --cert=public.crt --key=private.key
 
-# Deploy app 
+# Deploy app
 kubectl apply -f ../../extras/jwt/jwt-secret.yaml
-kubectl apply -f ../../kubernetes-manifests 
+kubectl apply -f ../../kubernetes-manifests
 
-# Delete frontend svc type loadbalancer 
-kubectl delete svc frontend 
-
-# Create service type nodeport 
-# (this will still allow in-cluster traffic from the loadgen because nodeport services automatically spawn cluster IP services)
-kubectl apply -f frontend-nodeport.yaml 
-
-
-# Apply frontend config to allow HTTP -> HTTPS redirect 
-kubectl apply -f frontend-config.yaml
-
-# Create ingress, referencing the cert secret and static IP 
-kubectl apply -f frontend-ingress.yaml
+######################################################################
+# This creates a Frontendconfig for HTTP->HTTPS redirection, modifies
+# the default frontend Service to be in Internal Loadbalancer
+# (default is External) and finally creates the Ingress that uses the
+# certificates created above
+######################################################################
+kubectl apply -f selfsigned-cert-ingress.yaml
