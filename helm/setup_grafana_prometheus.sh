55e2e7252ddc6e01daca32915f02010fe11200d1
Robert Starmer <rstarmer@gmail.com>
2022-05-11 18:00:54 -0700

    Add istio install script, and add metrics env. to istio

--- helm/setup_grafana_prometheus.sh
+++ helm/setup_grafana_prometheus.sh
@@ -6,13 +6,19 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 echo create the metrics namespace and install prometheus and grafana
-kubectl create ns metrics
+
+if [ ! "`kubectl get ns | grep metrics; echo $?`" ]; then
+ kubectl create ns metrics
+fi
+if [ ! "`kubectl get ns | grep istio-system; echo $?`" ]; then
+ kubectl label namespace metrics istio-injection=enabled --overwrite
+fi
 helm install prometheus prometheus-community/prometheus --namespace metrics
 helm install prom-adapter prometheus-community/prometheus-adapter --namespace metrics --values prom-adapter-values.yaml
 helm install grafana grafana/grafana --namespace metrics --values grafana-values.yaml
 
 #echo "Add additional metrics for prometheus"
-#kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
 echo "check metrics availability with: kubectl get --raw /apis/custom.metrics.k8s.io/v1beta1"
 
