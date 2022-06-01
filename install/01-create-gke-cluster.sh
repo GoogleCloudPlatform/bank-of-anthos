2f069e75272c3a992c83e8ecbb3105941af78096
Riccardo Carlesso <palladiusbonton+derek22@gmail.com>
2022-06-01 13:56:28 +0200

    deployed manifests and opening browser for lazy people too

--- install/01-create-gke-cluster.sh
+++ install/01-create-gke-cluster.sh
@@ -17,10 +17,22 @@ gcloud container clusters get-credentials bank-of-anthos \
   --project=${PROJECT_ID} --region=${REGION}
 
 
+# Alternatively, you can deploy using GKE Standard instead:
 
+# ```
+# ZONE=us-central1-b
+# gcloud beta container clusters create bank-of-anthos \
+#   --project=${PROJECT_ID} --zone=${ZONE} \
+#   --machine-type=e2-standard-2 --num-nodes=4 \
+#   --monitoring=SYSTEM --logging=SYSTEM,WORKLOAD --subnetwork=default \
+#   --tags=bank-of-anthos --labels csm=
+
+# gcloud container clusters get-credentials bank-of-anthos \
+#   --project=${PROJECT_ID} --zone=${ZONE}
+# ```
 
 
 
 
 # End of your code here
-verde Tutto ok.
+echo All good.
