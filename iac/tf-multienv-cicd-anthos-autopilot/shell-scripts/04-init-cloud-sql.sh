007156300664b77796091465244fa5d3db30550c
aablsk <bielski@google.com>
2022-10-06 14:21:45 +0000

    add shell scripts for easy setup

---
+++ iac/tf-multienv-cicd-anthos-autopilot/shell-scripts/04-init-cloud-sql.sh
@@ -0,0 +1,37 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -uo pipefail
+
+echo '🚀  Starting ./04-init-cloud-sql.sh'
+
+echo '🌱  Initializing staging db...'
+echo '🔑  Getting cluster credentials...'
+gcloud container clusters get-credentials staging --region=$REGION
+echo '🙌  Deploying populate-db jobs for staging...'
+skaffold config set default-repo $REGION-docker.pkg.dev/$PROJECT_ID/bank-of-anthos
+skaffold run --profile=init-db-staging
+echo '🕰  Wait for staging-db initialization to complete...'
+kubectl wait --for=condition=complete job/populate-accounts-db job/populate-ledger-db -n bank-of-anthos-staging --timeout=300s
+
+echo '🌱  Initializing production db...'
+echo '🔑  Getting cluster credentials...'
+gcloud container clusters get-credentials production --region=$REGION
+echo '🙌  Deploying populate-db jobs for staging...'
+skaffold config set default-repo $REGION-docker.pkg.dev/$PROJECT_ID/bank-of-anthos
+skaffold run --profile=init-db-production
+echo '🕰  Wait for production-db initialization to complete...'
+kubectl wait --for=condition=complete job/populate-accounts-db job/populate-ledger-db -n bank-of-anthos-production --timeout=300s
+
+echo '✅  Finished ./04-init-cloud-sql.sh'
---
+++ iac/tf-multienv-cicd-anthos-autopilot/shell-scripts/05-trigger-cicd.sh
@@ -0,0 +1,26 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -Eeuo pipefail
+
+echo '🚀  Starting ./05-trigger-cicd.sh'
+
+echo '🌈  Triggering CI/CD for Frontend team'
+gcloud beta builds triggers run frontend-ci --branch main
+echo '😁  Triggering CI/CD for Accounts team'
+gcloud beta builds triggers run accounts-ci --branch main
+echo '📒  Triggering CI/CD for Ledger team'
+gcloud beta builds triggers run ledger-ci --branch main
+
+echo '✅  Finished ./05-trigger-cicd.sh'
