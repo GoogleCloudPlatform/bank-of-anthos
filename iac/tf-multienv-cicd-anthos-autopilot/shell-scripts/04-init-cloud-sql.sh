6efba9c9d8309a9ef4b51ed3424850fd6ca0c7f1
aablsk <bielski@google.com>
2022-12-10 16:48:19 +0000

    fix issues, re-enable statuschecks

--- iac/tf-multienv-cicd-anthos-autopilot/shell-scripts/04-init-cloud-sql.sh
+++ iac/tf-multienv-cicd-anthos-autopilot/shell-scripts/04-init-cloud-sql.sh
@@ -27,7 +27,8 @@ echo '🔑  Getting cluster credentials...'
 gcloud container fleet memberships get-credentials staging-membership
 echo '🙌  Deploying populate-db jobs for staging...'
 /usr/local/bin/skaffold config set default-repo $REGION-docker.pkg.dev/$PROJECT_ID/bank-of-anthos
-/usr/local/bin/skaffold run --profile=init-db-staging
+/usr/local/bin/skaffold run --profile=init-db-staging --module=accounts-db
+/usr/local/bin/skaffold run --profile=init-db-staging --module=ledger-db
 echo '🕰  Wait for staging-db initialization to complete...'
 kubectl wait --for=condition=complete job/populate-accounts-db job/populate-ledger-db -n bank-of-anthos-staging --timeout=300s
 
@@ -36,7 +37,8 @@ echo '🔑  Getting cluster credentials...'
 gcloud container fleet memberships get-credentials production-membership
 echo '🙌  Deploying populate-db jobs for staging...'
 /usr/local/bin/skaffold config set default-repo $REGION-docker.pkg.dev/$PROJECT_ID/bank-of-anthos
-/usr/local/bin/skaffold run --profile=init-db-production
+/usr/local/bin/skaffold run --profile=init-db-production --module=accounts-db
+/usr/local/bin/skaffold run --profile=init-db-production --module=ledger-db
 echo '🕰  Wait for production-db initialization to complete...'
 kubectl wait --for=condition=complete job/populate-accounts-db job/populate-ledger-db -n bank-of-anthos-production --timeout=300s
 
