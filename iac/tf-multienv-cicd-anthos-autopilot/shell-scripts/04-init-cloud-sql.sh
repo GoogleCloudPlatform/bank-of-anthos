1306c13bfba8e8014ce4f565a28c6bf1dd1dfbca
aablsk <bielski@google.com>
2022-12-20 07:32:37 +0000

    fix sql db initialization

--- iac/tf-multienv-cicd-anthos-autopilot/shell-scripts/04-init-cloud-sql.sh
+++ iac/tf-multienv-cicd-anthos-autopilot/shell-scripts/04-init-cloud-sql.sh
@@ -20,15 +20,15 @@ echo '🌱  Initializing setting up development config...'
 echo '🔑  Getting cluster credentials...'
 gcloud container fleet memberships get-credentials development-membership
 echo '🙌  Setting default container registry for development...'
-/usr/local/bin/skaffold config set default-repo $REGION-docker.pkg.dev/$PROJECT_ID/bank-of-anthos
+skaffold config set default-repo $REGION-docker.pkg.dev/$PROJECT_ID/bank-of-anthos
 
 echo '🌱  Initializing staging db...'
 echo '🔑  Getting cluster credentials...'
 gcloud container fleet memberships get-credentials staging-membership
 echo '🙌  Deploying populate-db jobs for staging...'
-/usr/local/bin/skaffold config set default-repo $REGION-docker.pkg.dev/$PROJECT_ID/bank-of-anthos
-/usr/local/bin/skaffold run --profile=init-db-staging --module=accounts-db
-/usr/local/bin/skaffold run --profile=init-db-staging --module=ledger-db
+skaffold config set default-repo $REGION-docker.pkg.dev/$PROJECT_ID/bank-of-anthos
+skaffold run --profile=init-db-staging --module=accounts-db
+skaffold run --profile=init-db-staging --module=ledger-db
 echo '🕰  Wait for staging-db initialization to complete...'
 kubectl wait --for=condition=complete job/populate-accounts-db job/populate-ledger-db -n bank-of-anthos-staging --timeout=300s
 
@@ -36,9 +36,9 @@ echo '🌱  Initializing production db...'
 echo '🔑  Getting cluster credentials...'
 gcloud container fleet memberships get-credentials production-membership
 echo '🙌  Deploying populate-db jobs for staging...'
-/usr/local/bin/skaffold config set default-repo $REGION-docker.pkg.dev/$PROJECT_ID/bank-of-anthos
-/usr/local/bin/skaffold run --profile=init-db-production --module=accounts-db
-/usr/local/bin/skaffold run --profile=init-db-production --module=ledger-db
+skaffold config set default-repo $REGION-docker.pkg.dev/$PROJECT_ID/bank-of-anthos
+skaffold run --profile=init-db-production --module=accounts-db
+skaffold run --profile=init-db-production --module=ledger-db
 echo '🕰  Wait for production-db initialization to complete...'
 kubectl wait --for=condition=complete job/populate-accounts-db job/populate-ledger-db -n bank-of-anthos-production --timeout=300s
 
