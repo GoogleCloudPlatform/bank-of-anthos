8d8d32401cb736414a7fff2c71173d6afe3a5306
aablsk <bielski@google.com>
2023-01-01 17:08:14 +0000

    fix E2E tests

--- .github/workflows/ui-tests/run_for_env.sh
+++ .github/workflows/ui-tests/run_for_env.sh
@@ -20,9 +20,9 @@ if [[ -z "${CYPRESS_baseUrl}" ]]; then
     export ARTIFACTS_BUCKET_NAME=gs://delivery-artifacts-$PIPELINE-$PROJECT_ID
     gcloud container fleet memberships get-credentials $ANTHOS_MEMBERSHIP_SHORT
     if [[ "$ANTHOS_MEMBERSHIP_SHORT" == "staging-membership" ]]; then
-        export CYPRESS_baseUrl=$(kubectl get service frontend --namespace bank-of-anthos-staging -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+        export CYPRESS_baseUrl=http://$(kubectl get service frontend --namespace bank-of-anthos-staging -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
     elif [[ "$ANTHOS_MEMBERSHIP_SHORT" == "production-membership" ]]; then
-        export CYPRESS_baseUrl=$(kubectl get ingress frontend-ingress --namespace bank-of-anthos-production -o jsonpath='{.status.loadBalancer.ingress[0].ip}') # TODO: consider switching to domain name
+        export CYPRESS_baseUrl=http://$(kubectl get ingress frontend-ingress --namespace bank-of-anthos-production -o jsonpath='{.status.loadBalancer.ingress[0].ip}') # TODO: consider switching to domain name
     else
         echo ERROR: CYPRESS_baseUrl is not set and cannot be automatically determined. Exiting with status code 1.
         exit 1
