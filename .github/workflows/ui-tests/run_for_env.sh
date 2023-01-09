a6b55075f2fa199883584c0d7de3ed568e3bb19d
aablsk <bielski@google.com>
2023-01-09 07:49:44 +0000

    fix e2e tests for production

--- .github/workflows/ui-tests/run_for_env.sh
+++ .github/workflows/ui-tests/run_for_env.sh
@@ -24,7 +24,7 @@ if [[ -z "${CYPRESS_baseUrl}" ]]; then
     if [[ "$ANTHOS_MEMBERSHIP_SHORT" == "staging-membership" ]]; then
         export CYPRESS_baseUrl=http://$(kubectl get service frontend --namespace bank-of-anthos-staging -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
     elif [[ "$ANTHOS_MEMBERSHIP_SHORT" == "production-membership" ]]; then
-        export CYPRESS_baseUrl=http://$(kubectl get ingress frontend-ingress --namespace bank-of-anthos-production -o jsonpath='{.status.loadBalancer.ingress[0].ip}') # TODO: consider switching to domain name
+        export CYPRESS_baseUrl=https://bank-of-anthos.bielski.demo.altostrat.com
     else
         echo ERROR: CYPRESS_baseUrl is not set and cannot be automatically determined. Exiting with status code 1.
         exit 1
@@ -37,7 +37,7 @@ CYPRESS_baseUrl=$CYPRESS_baseUrl NO_COLOR=1 cypress run --reporter json-stream
 # if failed, copy screenshots to bucket and exit with status code 1
 if [[ "$?" -ne 0 ]]; then
     export COPY_DESTINATION=$ARTIFACTS_BUCKET_NAME/$ROLLOUT/e2e/cypress/
-    echo ERROR: Cypress E2E tests have failed. Screenshots will be uploaded to $COPY_DESTINATION/screenshots 
+    echo ERROR: Cypress E2E tests have failed. Screenshots will be uploaded to $COPY_DESTINATIONscreenshots 
     gcloud storage cp -r /e2e/cypress/screenshots $COPY_DESTINATION
     exit 1
 fi
