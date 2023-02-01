2ec9a2a11d70f0b045e1891c1b9f214f3e37347a
aaronmwanjala <aaronmwanjala@google.com>
2023-02-01 16:03:49 -0500

    refactor: Replace javax.* imports with jakarta.* counterparts

--- src/transactionhistory/src/main/java/anthos/samples/bankofanthos/transactionhistory/Transaction.java
+++ src/transactionhistory/src/main/java/anthos/samples/bankofanthos/transactionhistory/Transaction.java
@@ -16,18 +16,15 @@
 
 package anthos.samples.bankofanthos.transactionhistory;
 
-import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.CreationTimestamp;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Date;
+import org.hibernate.annotations.CreationTimestamp;
 
 /**
  * Defines a banking transaction.
