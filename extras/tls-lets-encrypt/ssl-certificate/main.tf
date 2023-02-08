11367227f858c09c4f78946cd1cfb16f66aa46b6
Igor Lysak <igor_lysak@epam.com>
2023-02-08 17:54:41 +0200

    Using Let's Encrypt staging env while WIP.

--- extras/tls-lets-encrypt/ssl-certificate/main.tf
+++ extras/tls-lets-encrypt/ssl-certificate/main.tf
@@ -26,8 +26,8 @@ terraform {
 provider "google" {}
 
 provider "acme" {
-  # server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  # server_url = "https://acme-v02.api.letsencrypt.org/directory"
 }
 
 variable "domin_name" {
