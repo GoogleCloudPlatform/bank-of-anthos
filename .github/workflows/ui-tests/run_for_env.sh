cf77ef1e144cdb29fb5c14642bb87cd2422ab1dd
aablsk <bielski@google.com>
2023-03-15 09:26:22 +0000

    fix screenshot upload to bucket

--- .github/workflows/ui-tests/run_for_env.sh
+++ .github/workflows/ui-tests/run_for_env.sh
@@ -18,8 +18,7 @@ gcloud auth list
 if [[ -z "${CYPRESS_baseUrl}" ]]; then
     # Get credentials for current Anthos cluster (staging/production)
     export ANTHOS_MEMBERSHIP_SHORT=$(echo $ANTHOS_MEMBERSHIP | cut -d/ -f6)
-    export PROJECT_ID=$(echo $ANTHOS_MEMBERSHIP | cut -d/ -f2)
-    export ARTIFACTS_BUCKET_NAME=gs://delivery-artifacts-$PIPELINE-$PROJECT_ID
+    export ARTIFACTS_BUCKET_NAME=gs://delivery-artifacts-$PIPELINE-$PROJECT
     gcloud container fleet memberships get-credentials $ANTHOS_MEMBERSHIP_SHORT
     if [[ "$ANTHOS_MEMBERSHIP_SHORT" == "staging-membership" ]]; then
         export CYPRESS_baseUrl=http://$(kubectl get service frontend --namespace bank-of-anthos-staging -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
@@ -37,7 +36,7 @@ CYPRESS_baseUrl=$CYPRESS_baseUrl NO_COLOR=1 cypress run --reporter json-stream -
 # if failed, copy screenshots to bucket and exit with status code 1
 if [[ "$?" -ne 0 ]]; then
     export COPY_DESTINATION=$ARTIFACTS_BUCKET_NAME/$ROLLOUT/e2e/cypress/
-    echo ERROR: Cypress E2E tests have failed. Screenshots will be uploaded to $COPY_DESTINATIONscreenshots 
+    echo ERROR: Cypress E2E tests have failed. Screenshots will be uploaded to $COPY_DESTINATION screenshots 
     gcloud storage cp -r /e2e/cypress/screenshots $COPY_DESTINATION
     exit 1
 fi
