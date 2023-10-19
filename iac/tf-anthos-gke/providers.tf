14040826811453dd04bcdcc5a8e2b7d152343ee5
Olivier Bourgeois <3271352+bourgeoisor@users.noreply.github.com>
2023-10-19 13:38:49 -0400

    Update providers.tf

--- iac/tf-anthos-gke/providers.tf
+++ iac/tf-anthos-gke/providers.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 5.2"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.0"
+      version = "~> 5.2"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
