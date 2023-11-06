5a43dbbf6aabc3629ab36588448594fc3f831e9b
Olivier Bourgeois <bourgeoisor@gmail.com>
2023-11-06 16:06:30 -0500

    Bump tf providers and modules

--- iac/tf-multienv-cicd-anthos-autopilot/env-staging.tf
+++ iac/tf-multienv-cicd-anthos-autopilot/env-staging.tf
@@ -23,6 +23,7 @@ provider "kubernetes" {
 # staging autopilot cluster
 module "gke_staging" {
   source = "terraform-google-modules/kubernetes-engine/google//modules/beta-autopilot-private-cluster"
+  version = "29.0.0"
 
   project_id              = var.project_id
   name                    = "staging"
