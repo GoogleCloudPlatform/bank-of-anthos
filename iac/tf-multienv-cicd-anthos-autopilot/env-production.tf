a8cf57260d1485e85583e800e5ec6b27079fd7f3
henrybell <henrybell@users.noreply.github.com>
2024-08-16 15:39:35 +0000

    fix: Pin postgresql module to known good version

--- iac/tf-multienv-cicd-anthos-autopilot/env-production.tf
+++ iac/tf-multienv-cicd-anthos-autopilot/env-production.tf
@@ -73,9 +73,10 @@ resource "google_service_account_iam_member" "gke_workload_production_identity" 
   ]
 }
 
-# CloudSQL Postgres production instance 
+# CloudSQL Postgres production instance
 module "cloudsql_production" {
-  source = "GoogleCloudPlatform/sql-db/google//modules/postgresql"
+  source  = "GoogleCloudPlatform/sql-db/google//modules/postgresql"
+  version = "~> 20.0.0"
 
   project_id = var.project_id
   region     = var.region
