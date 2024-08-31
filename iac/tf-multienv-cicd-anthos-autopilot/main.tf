8dbbc1c84a6422edd3792763a9f1690e9a55b2c3
Henry Bell <henrybell@users.noreply.github.com>
2024-08-31 18:20:02 +0100

    fix: Pin Google Terraform provider to known good version (#2201)

--- iac/tf-multienv-cicd-anthos-autopilot/main.tf
+++ iac/tf-multienv-cicd-anthos-autopilot/main.tf
@@ -19,10 +19,12 @@ terraform {
   }
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
+      version = "~> 5.40.0"
     }
     google-beta = {
-      source = "hashicorp/google-beta"
+      source  = "hashicorp/google-beta"
+      version = "~> 5.40.0"
     }
   }
 }
