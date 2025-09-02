f16d1cf0965e67f195b43f478f9e2d6d8c7055de
Edima <edimaudo@gmail.com>
2025-09-02 13:14:17 -0400

    Update logger.py

--- insights_service/app/utils/logger.py
+++ insights_service/app/utils/logger.py
@@ -1,1 +1,10 @@
 
+import logging
+
+logger = logging.getLogger("insights_service")
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
