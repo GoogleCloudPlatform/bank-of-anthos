9326d4e80843e4dc858755dfa5d31052b72481e9
Edima <edimaudo@gmail.com>
2025-09-02 13:36:34 -0400

    Update SpendingChart.tsx

--- insights_service/frontend/src/components/FinancialHealth/SpendingChart.tsx
+++ insights_service/frontend/src/components/FinancialHealth/SpendingChart.tsx
@@ -1,1 +1,20 @@
 
+import React from "react";
+import { Bar } from "react-chartjs-2";
+
+const SpendingChart = ({ data }: { data: Record<string, number> }) => {
+  const chartData = {
+    labels: Object.keys(data),
+    datasets: [
+      {
+        label: "Spending Breakdown",
+        data: Object.values(data),
+        backgroundColor: "rgba(54, 162, 235, 0.6)",
+      },
+    ],
+  };
+
+  return <Bar data={chartData} />;
+};
+
+export default SpendingChart;
