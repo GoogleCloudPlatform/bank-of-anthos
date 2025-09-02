f8de357acc57d8458004375f8e0d6a598000bd26
Edima <edimaudo@gmail.com>
2025-09-02 13:35:56 -0400

    Update insightsApi.ts

--- insights_service/frontend/src/components/services/insightsApi.ts
+++ insights_service/frontend/src/components/services/insightsApi.ts
@@ -1,1 +1,9 @@
 
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_INSIGHTS_API || "http://localhost:8080/api/v1/insights";
+
+export const fetchInsights = async (userId: string) => {
+  const response = await axios.get(`${BASE_URL}/${userId}`);
+  return response.data;
+};
