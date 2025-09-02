cd4690b11f5a8eeb6bb8404594038ead9ec9e546
Edima <edimaudo@gmail.com>
2025-09-02 13:36:50 -0400

    Update CreditUtilizationGauge.tsx

--- insights_service/frontend/src/components/FinancialHealth/CreditUtilizationGauge.tsx
+++ insights_service/frontend/src/components/FinancialHealth/CreditUtilizationGauge.tsx
@@ -1,1 +1,15 @@
 
+import React from "react";
+import { CircularProgress } from "@mui/material";
+
+const CreditUtilizationGauge = ({ value }: { value: number }) => {
+  const percent = Math.round(value * 100);
+  return (
+    <div>
+      <p>Credit Utilization: {percent}%</p>
+      <CircularProgress variant="determinate" value={percent} />
+    </div>
+  );
+};
+
+export default CreditUtilizationGauge;
