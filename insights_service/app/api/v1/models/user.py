ecd011eca394ab0fd4383c6ab7e8ae546fb6d991
Edima <edimaudo@gmail.com>
2025-09-02 13:13:23 -0400

    Update user.py

--- insights_service/app/api/v1/models/user.py
+++ insights_service/app/api/v1/models/user.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel
+from typing import List, Dict
+
+class Transaction(BaseModel):
+    category: str
+    amount: float
+
+class UserData(BaseModel):
+    user_id: str
+    income: float
+    transactions: List[Transaction]
+    credit_limit: float
+    current_balance: float
 
