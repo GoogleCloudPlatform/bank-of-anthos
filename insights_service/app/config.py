c7b78b2e1236fa8c26763393a31b7eba8478f374
Edima <edimaudo@gmail.com>
2025-09-02 13:14:31 -0400

    Update config.py

--- insights_service/app/config.py
+++ insights_service/app/config.py
@@ -1,1 +1,12 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = "Insights Service"
+    boa_base_url: str = "http://bank-of-anthos-service"
+    cache_ttl_seconds: int = 600
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
 
