12b3f9db17437dff59467ef63a368df54b27ee43
Olivier Bourgeois <3271352+bourgeoisor@users.noreply.github.com>
2025-12-31 16:58:19 -0500

    Upgrade Google provider versions to 7.14.1

--- iac/tf-multienv-cicd-anthos-autopilot/main.tf
+++ iac/tf-multienv-cicd-anthos-autopilot/main.tf
@@ -20,11 +20,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.43.0"
+      version = "~> 7.14.1"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 5.43.0"
+      version = "~> 7.14.1"
     }
   }
 }
